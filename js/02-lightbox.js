ba0e27e9695f0f1a4a3bb922281197100d7e6a86
Marina Boikova <boymar84@gmail.com>
2021-11-10 23:20:15 +0200

    Добавляет решение Задачи 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,26 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryItemsRef = document.querySelector('.gallery');
+
+const galleryItemMarcup = galleryItems.map(item => 
+    `<a class="gallery__item" href="${item.original}">
+    <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+  </a> `
+);
+
+galleryItemsRef.insertAdjacentHTML('beforeend',  galleryItemMarcup.join(''));
+
+const onHandleLargePhoto = e => 
+    e.preventDefault();
+   let lightbox = new SimpleLightbox('.gallery a', { 
+    captionType: "attr",
+    captionsData: "alt",
+    captionDelay: 250
+    });
+
+
+
+
+galleryItemsRef.addEventListener('click', onHandleLargePhoto);
